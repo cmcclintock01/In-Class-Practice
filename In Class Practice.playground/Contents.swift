48034b545e0050311a7f7fff2c204a0a485a4cc7
Corben McClintock <99354075+cmcclintock01@users.noreply.github.com>
2023-01-19 10:44:10 -0600

    q

--- In Class Practice.playground/Contents.swift
+++ In Class Practice.playground/Contents.swift
@@ -20,6 +20,46 @@ var origin = (x : 0, y : 0)
 var point = origin
 print("", terminator: "(")
 print(point.x, point.y, separator: ",", terminator: ")")
+print("")
 
-let city = (name : "Maryville", population : 11,000)
+let groceries = ("bread","onions")
+print(groceries.0)
+print(groceries.1)
+print(type(of: groceries))
 
+var cricketKit = ("handGloves" ,"helmet", ("bat", "ball"), "😀")
+print(cricketKit.0)
+print(cricketKit.1)
+print(cricketKit.2.0)
+print(cricketKit.2)
+print(cricketKit.2.0, cricketKit.1, cricketKit.3)
+
+var firstN : Int = 10
+var secondN : Int = 25
+print(secondN / firstN)
+
+var number1 = (4,5)
+var number2 = (4,5)
+print(number1 == number2)
+
+number1 = (3,5)
+number2 = (4,5)
+print(number1 == number2)
+
+number1 = (4,6)
+number2 = (4,5)
+print(number1 == number2)
+
+var four = 4
+var finalNumber = -four
+print(finalNumber)
+
+var x : Int = 12
+var y : Int  = 7
+var z : Int  = 5
+var a : Bool = true
+var b : Bool = false
+print(x * z > z * y || b)
+print(x * z < z * y && a)
+print(x * z > z * y && a || b)
+print(!(x * z > z * y && a || b))
